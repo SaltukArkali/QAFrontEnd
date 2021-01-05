f6b26d8fadf5ca583e45b15e7ffc53aa0eb276e4
Saltuk <saltukarkali@gmail.com>
2021-01-05 01:18:15 +0100

    local saltuk commit

---
+++ src/test/java/pages/KitapyurduPagesSBA.java
@@ -0,0 +1,76 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class KitapyurduPagesSBA {
+    public KitapyurduPagesSBA(WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
+    @FindBy(xpath = "(//span[@class='RveJvd snByac'])[3]")
+    public WebElement ichStimmeZu;
+
+    @FindBy(xpath = "//input[@name='q']")
+    public WebElement searchBox;
+
+    @FindBy(xpath = "//a[@href='https://www.kitapyurdu.com/']")
+    public WebElement kitapYurduLink;
+
+    @FindBy(xpath = "//span[@id='cart-items']")
+    public WebElement sepetLink;
+
+    @FindBy(xpath = "//*[@class='empty']")
+    public WebElement sepetAciklama;
+
+    @FindBy(xpath = "//span[@class='mn-strong common-sprite']")
+    public WebElement enCokSatanLink;
+
+    @FindBy(xpath = "//a[@href='kategori/kitap/1.html']")
+    public WebElement kitapEnCokSatan;
+
+    @FindBy(xpath = "//li[@class='mg-b-10']")
+    public List<WebElement> cokSatanList;
+
+    @FindBy(xpath = "//*[.='Daha Sonra']")
+    public WebElement alertIgnore;
+
+    @FindBy(id = "search-input")
+    public WebElement searchBoxAbove;
+
+    @FindBy(xpath = "//span[.='Suç ve Ceza 1']")
+    public WebElement sucVeCeza1;
+
+    @FindBy(xpath = "(//span[@class='value'])[12]")
+    public WebElement sucVeCeza1Fiyat;
+
+    @FindBy(xpath = "//span[.='Suç ve Ceza 2']")
+    public WebElement sucVeCeza2;
+
+    @FindBy(xpath = "(//span[@class='value'])[14]")
+    public WebElement sucVeCeza2Fiyat;
+
+    @FindBy(xpath = "(//*[@class='fa fa-shopping-cart'])[12]")
+    public WebElement sepeteEkle12;
+
+    @FindBy(xpath = "(//*[@class='fa fa-shopping-cart'])[14]")
+    public WebElement sepeteEkle14;
+
+    @FindBy(id = "cart-items")
+    public WebElement sepet;
+
+    @FindBy(id = "cart-content")
+    public WebElement sepetIcerik;
+
+
+ //   mg-b-10
+
+
+
+
+
+
+}
---
+++ src/test/java/tests/KitapyurduTestsSBA.java
@@ -0,0 +1,84 @@
+package tests;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.KitapyurduPagesSBA;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class KitapyurduTestsSBA {
+
+    KitapyurduPagesSBA pages = new KitapyurduPagesSBA(Driver.getDriver());
+    Actions actions = new Actions(Driver.getDriver());
+
+    @Test
+    public void deneme(){
+        Driver.getDriver().get(ConfigReader.getProperty("google"));
+        ReusableMethods.waitFor(5);
+        Driver.getDriver().switchTo().frame(0);
+        pages.ichStimmeZu.click();
+        pages.searchBox.sendKeys("kitap yurdu"+ Keys.ENTER);
+        ReusableMethods.waitFor(4);
+        pages.kitapYurduLink.click();
+        ReusableMethods.waitFor(2);
+
+        String title = Driver.getDriver().getTitle();
+        System.out.println(title);
+        Assert.assertEquals(title,"Kitapyurdu, Kitapla buluşmanın en kolay yolu");
+
+        pages.sepetLink.click();
+        ReusableMethods.waitFor(2);
+        String sepetAciklama = pages.sepetAciklama.getText();
+        System.out.println(sepetAciklama);
+        Assert.assertEquals(sepetAciklama,"Alışveriş sepetiniz boş!");
+
+        pages.enCokSatanLink.click();
+        ReusableMethods.waitFor(1);
+        pages.kitapEnCokSatan.click();
+        ReusableMethods.waitFor(2);
+
+        for(int i=0;i<10;i++) {
+            String bookName = pages.cokSatanList.get(i).getText();
+            System.out.println(bookName);
+        }
+
+        String enCokSatanKitap = pages.cokSatanList.get(0).getText();
+        System.out.println(enCokSatanKitap);
+        Assert.assertTrue(enCokSatanKitap.contains("Körlük"));
+
+        pages.alertIgnore.click();
+        ReusableMethods.waitFor(2);
+
+        pages.searchBoxAbove.sendKeys("suç ve ceza"+Keys.ENTER);
+        ReusableMethods.waitFor(3);
+
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        ReusableMethods.waitFor(2);
+        Assert.assertEquals(pages.sucVeCeza1.getText(),"Suç ve Ceza 1");
+        ReusableMethods.waitFor(3);
+        Assert.assertEquals(pages.sucVeCeza2.getText(),"Suç ve Ceza 2");
+
+         ReusableMethods.waitFor(1);
+//        String abc = pages.sucVeCeza1Fiyat.getAttribute("/span");
+//        System.out.println(abc);
+////        Assert.assertEquals(pages.sucVeCeza1Fiyat.getText(),"9.00");
+////        ReusableMethods.waitFor(1);
+////        Assert.assertEquals(pages.sucVeCeza2Fiyat.getText(),"7.49");
+
+        pages.sepeteEkle12.click();
+        ReusableMethods.waitFor(2);
+        pages.sepeteEkle14.click();
+        ReusableMethods.waitFor(2);
+
+        pages.sepet.click();
+        String sepetAyrinti = pages.sepetIcerik.getText();
+        System.out.println(sepetAyrinti);
+        Assert.assertTrue(sepetAyrinti.contains("Suç ve Ceza 1"));
+        Assert.assertTrue(sepetAyrinti.contains("Suç ve Ceza 2"));
+
+    }
+
+}
